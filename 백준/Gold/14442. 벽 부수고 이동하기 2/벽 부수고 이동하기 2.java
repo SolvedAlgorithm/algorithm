c7f7348d5dfa77968993d417caa5cd0d87c08774
seunghee <43427305+seunghee114@users.noreply.github.com>
2023-10-13 16:25:32 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 2236 ms, Memory: 407088 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
     static int N, M, K;
@@ -29,7 +31,7 @@ public class Main {
         }
         // end input
 
-        dijkstra();
+        bfs();
         int answer = Integer.MAX_VALUE;
         for (int k = 0; k < K + 1; k++) {
             answer = Math.min(answer, Dist[N - 1][M - 1][k]);
@@ -41,31 +43,33 @@ public class Main {
     static int[] di = {-1, 1, 0, 0};
     static int[] dj = {0, 0, 1, -1};
 
-    static void dijkstra() {
-        PriorityQueue<int[]> PQ = new PriorityQueue<>((o1, o2)->Integer.compare(o1[2], o2[2]));
+    static void bfs() {
+        Queue<int[]> Q = new ArrayDeque<>();
+        boolean[][][] visit = new boolean[N][M][K + 1];
+        // Dist[i][j][k] : (i, j)까지 가는데 k번 벽을 부쉈을 때 최단 거리
         Dist[0][0][0] = 1;
+        visit[0][0][0] = true;
         // (0, 1) 위치, 2 거리, 3 부순 벽의 개수
-        PQ.add(new int[]{0, 0, 1, 0});
-        while (!PQ.isEmpty()) {
-            int[] tp = PQ.poll();
-            // 이미 최단 거리면 continue
-            if (Dist[tp[0]][tp[1]][tp[3]] < tp[2]) continue;
+        Q.add(new int[]{0, 0, 1, 0});
+        while (!Q.isEmpty()) {
+            int[] tp = Q.poll();
             for (int k = 0; k < 4; k++) {
                 int ni = tp[0] + di[k];
                 int nj = tp[1] + dj[k];
                 if (ni < 0 || nj < 0 || ni >= N || nj >= M) continue;
                 if (Board[ni][nj] == 1) {   // 벽이면 부숴야 함
-                    if (tp[3] < K) {    // 지금까지 부순 벽의 개수가 K개 이하면 부술 수 있음
-                        if (Dist[ni][nj][tp[3] + 1] <= tp[2]+1) continue;
-                        Dist[ni][nj][tp[3]+1] = tp[2] + 1;
-                        PQ.add(new int[]{ni, nj, tp[2] + 1, tp[3] + 1});
-                    }
+                    // 이미 방문했거나, 벽을 이미 K개 부쉈으면
+                    if (tp[3] >= K || visit[ni][nj][tp[3]+1]) continue;
+                    Dist[ni][nj][tp[3]+1] = tp[2] + 1;
+                    Q.add(new int[]{ni, nj, tp[2] + 1, tp[3] + 1});
+                    visit[ni][nj][tp[3] + 1] = true;
                     continue;
                 }
                 // 벽이 아니면 그냥 갈 수 있음
-                if (Dist[ni][nj][tp[3]] <= tp[2] + 1) continue;
+                if (visit[ni][nj][tp[3]]) continue;     // 이미 방문 했으면
                 Dist[ni][nj][tp[3]] = tp[2] +1;
-                PQ.add(new int[]{ni, nj, tp[2] + 1, tp[3]});
+                Q.add(new int[]{ni, nj, tp[2] + 1, tp[3]});
+                visit[ni][nj][tp[3]] = true;
             }
         }
     }
