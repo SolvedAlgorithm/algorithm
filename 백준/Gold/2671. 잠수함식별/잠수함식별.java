63ccbd288308dd070c0b923e5bdebeac16d980a6
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-23 16:17:55 +0900

    [Gold V] Title: 잠수함식별, Time: 120 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.java
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.java
@@ -1,50 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         StringBuilder sb = new StringBuilder();
+        String REGEXP = "(100+1+|01)+";
         // eof를 만날 때까지
         while (str != null && !str.equals("")){
-            char[] arr = str.toCharArray();
-            sb.append(check(arr) ? "SUBMARINE" : "NOISE").append("\n");
+            boolean chk = Pattern.matches(REGEXP, str);
+            sb.append(chk ? "SUBMARINE" : "NOISE").append("\n");
             str = br.readLine();
         }
         System.out.println(sb);
-    }
-    static boolean check(char[] arr) {
-        int N = arr.length;
-        boolean chk_100 = false;
-        boolean chk_1 = false;
-        for (int idx = 0; idx < N; idx++) {
-            if (arr[idx] == '1') {  // 1, 100, 101 중 1개
-                if (idx + 2 < N && arr[idx + 1] == '0' && arr[idx+2] == '0') { // 100
-                    chk_1 = false;
-                    idx += 2;
-                    if (chk_100) return false;
-                    chk_100 = true;
-                    while (idx+1 < N && arr[idx+1] == '0') {    // 0은 반복 가능
-                        idx++;
-                    }
-                    continue;
-                }
-                if (!chk_100 && !chk_1) return false;
-                chk_100 = false;
-                chk_1 = true;
-            } else {    // arr[idx] == '0'
-                if (chk_100) return false;
-
-                if (idx + 1 < N && arr[idx + 1] == '1') {
-                    chk_1 = false;
-                    idx += 1;
-                    continue;
-                }
-                return false;
-            }
-        }
-        return !chk_100;
     }
 }
